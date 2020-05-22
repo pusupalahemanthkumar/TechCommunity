49620473d8b6e3062cb24e53546b2d05ee1a5aa0
hemanth kumar <rockzzhemanth745@gmail.com>
2020-05-22 12:53:37 +0530

    Changes to be committed:

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Importing Required Files And Packages Here.
 const express = require("express");
+const path= require("path");
 
 const connectDB = require("./config/db");
 
@@ -20,6 +21,19 @@ app.use("/api/users", require("./routes/api/user"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts",require("./routes/api/post"));
 app.use("/api/profile",require("./routes/api/profile"));
+
+// Server static assets in production
+if(process.env.NODE_ENV === "production"){
+  // set static folder
+  app.use(express.static(path.join(__dirname,"client","build")));
+
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+
+  })
+
+}
+
 
 // Setting Listen Handler Here.
 app.listen(PORT, () => {
